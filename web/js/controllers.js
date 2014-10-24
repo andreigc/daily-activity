69eb89cee08eb1edd2104ab65e8d0d42f095bf6b
Andrei Geacar <andrei.geacar@gmail.com>
2014-10-24 14:11:09 +0300

    Frontend: Changed the way priority is displayed in the tasklist

--- web/js/controllers.js
+++ web/js/controllers.js
@@ -1,48 +1,75 @@
-var dailyAgendaApp =  angular.module('dailyAgendaApp',['ngCookies']);
+var dailyAgendaApp = angular.module('dailyAgendaApp', [ 'ngCookies' ]);
 
-dailyAgendaApp.config(function ($httpProvider) {
-    $httpProvider.defaults.withCredentials = true;
+dailyAgendaApp.config(function($httpProvider) {
+	$httpProvider.defaults.withCredentials = true;
+});
+
+dailyAgendaApp.filter('priority', function() {
+	return function(input) {
+		switch (input) {
+		case 1:
+			return 'low'
+		case 2:
+			return 'important'
+		case 3:
+			return 'urgent'
+		default:
+			return 'urgent'
+		}
+		
+	};
 });
 
 dailyAgendaApp.directive('ngHoverTask', function() {
-	  return {
-	    link: function(scope, element, attributes) {
-	       var id  = attributes.id;
-	       var type = attributes.type;
-	       element.bind('mouseenter', function() {
-	    	  if(type==2){
-	    		  angular.element(document.querySelector('.new-subtask-'+id)).removeClass('hidden');
-	    	  }
-	       })
-	        element.bind('mouseleave', function() {
-	          if(type==2){
-	        	  angular.element(document.querySelector('.new-subtask-'+id)).addClass('hidden');
-	         }
-	      })
-	    }
-	  }
+	return {
+		link : function(scope, element, attributes) {
+			var id = attributes.id;
+			var type = attributes.type;
+			element.bind('mouseenter', function() {
+				if (type == 2) {
+					angular.element(
+							document.querySelector('.new-subtask-' + id))
+							.removeClass('hidden');
+				}
+			})
+			element.bind('mouseleave', function() {
+				if (type == 2) {
+					angular.element(
+							document.querySelector('.new-subtask-' + id))
+							.addClass('hidden');
+				}
+			})
+		}
+	}
 });
-
 
 dailyAgendaApp.directive('ngHoverCategory', function() {
-	  return {
-	    link: function(scope, element, attributes) {
-	       var id  = attributes.id;
-	       element.bind('mouseenter', function() {
-	    	   angular.element(document.querySelector('.new-task-'+id)).removeClass('hidden');
-	       })
-	        element.bind('mouseleave', function() {
-	           angular.element(document.querySelector('.new-task-'+id)).addClass('hidden');
-	      })
-	    }
-	  }
+	return {
+		link : function(scope, element, attributes) {
+			var id = attributes.id;
+			element.bind('mouseenter', function() {
+				angular.element(document.querySelector('.new-task-' + id))
+						.removeClass('hidden');
+			})
+			element.bind('mouseleave', function() {
+				angular.element(document.querySelector('.new-task-' + id))
+						.addClass('hidden');
+			})
+		}
+	}
 });
 
-dailyAgendaApp.controller('TaskListController',['$scope','$http','$cookies',function($scope,$http,$cookies){
-	$cookies.sessionId=1;
-	$http.get("rest/tasks/get/multiple?userId=1").success(function(data){ $scope.categories = data;});
-	
-	$scope.orderProp = '-priority';
-	
-	
-}]);
+dailyAgendaApp.controller('TaskListController', [
+		'$scope',
+		'$http',
+		'$cookies',
+		function($scope, $http, $cookies) {
+			$cookies.sessionId = 1;
+			$http.get("rest/tasks/get/multiple?userId=1").success(
+					function(data) {
+						$scope.categories = data;
+					});
+
+			$scope.orderProp = '-priority';
+
+		} ]);
