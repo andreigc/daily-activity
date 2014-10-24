04626d3c9c856ffc0f6f310cf33409f1d637edb0
Andrei Geacar <andrei.geacar@gmail.com>
2014-10-24 12:53:11 +0300

    Added front-end functionality for displaying the progress of tasks and a starting point for adding tasks and sub-tasks. Also tasks are now sorted by priority.

--- web/js/controllers.js
+++ web/js/controllers.js
@@ -4,7 +4,45 @@ dailyAgendaApp.config(function ($httpProvider) {
     $httpProvider.defaults.withCredentials = true;
 });
 
+dailyAgendaApp.directive('ngHoverTask', function() {
+	  return {
+	    link: function(scope, element, attributes) {
+	       var id  = attributes.id;
+	       var type = attributes.type;
+	       element.bind('mouseenter', function() {
+	    	  if(type==2){
+	    		  angular.element(document.querySelector('.new-subtask-'+id)).removeClass('hidden');
+	    	  }
+	       })
+	        element.bind('mouseleave', function() {
+	          if(type==2){
+	        	  angular.element(document.querySelector('.new-subtask-'+id)).addClass('hidden');
+	         }
+	      })
+	    }
+	  }
+});
+
+
+dailyAgendaApp.directive('ngHoverCategory', function() {
+	  return {
+	    link: function(scope, element, attributes) {
+	       var id  = attributes.id;
+	       element.bind('mouseenter', function() {
+	    	   angular.element(document.querySelector('.new-task-'+id)).removeClass('hidden');
+	       })
+	        element.bind('mouseleave', function() {
+	           angular.element(document.querySelector('.new-task-'+id)).addClass('hidden');
+	      })
+	    }
+	  }
+});
+
 dailyAgendaApp.controller('TaskListController',['$scope','$http','$cookies',function($scope,$http,$cookies){
 	$cookies.sessionId=1;
 	$http.get("rest/tasks/get/multiple?userId=1").success(function(data){ $scope.categories = data;});
+	
+	$scope.orderProp = '-priority';
+	
+	
 }]);
