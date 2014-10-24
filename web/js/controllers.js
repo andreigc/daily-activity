2e6f7a1f16ebc0c905601b7aa91493ff485e7553
Andrei Geacar <andrei.geacar@gmail.com>
2014-10-24 17:34:13 +0300

    Added priority select on the new task page

--- web/js/controllers.js
+++ web/js/controllers.js
@@ -4,7 +4,8 @@ dailyAppcontrollers.controller('TaskListController', [
 		'$scope',
 		'$http',
 		'$cookies',
-		'$location',		function($scope, $http, $cookies, $location) {
+		'$location',
+		function($scope, $http, $cookies, $location) {
 			$cookies.sessionId = 1;
 			$http.get("rest/tasks/get/multiple?userId=1").success(
 					function(data) {
@@ -19,21 +20,35 @@ dailyAppcontrollers.controller('TaskListController', [
 
 		} ]);
 
-dailyAppcontrollers.controller('TaskNewController', [ '$scope', '$routeParams', '$http', '$cookies', function($scope, $routeParams, $http, $cookies) {
+dailyAppcontrollers.controller('TaskNewController', [ '$scope', '$routeParams',
+		'$http', '$cookies', function($scope, $routeParams, $http, $cookies) {
 			$scope.task = {}
 			if ($routeParams.parentId) {
 				$scope.task.parentId = $routeParams.parentId;
 			}
 			$cookies.sessionId = 1;
-			$http.get("rest/categories/get").success(
-					function(data) {
-						$scope.categories = data;
-						$scope.category=$scope.categories[0];
-						$scope.task.categoryId = $scope.category.id;
-					});
-			
+			$http.get("rest/categories/get").success(function(data) {
+				$scope.categories = data;
+				$scope.category = $scope.categories[0];
+				$scope.task.categoryId = $scope.category.id;
+			});
+
+			$scope.priorities = [ {
+				name : 'low',
+				value : 1
+			}, {
+				name : 'important',
+				value : 2
+			}, {
+				name : 'urgent',
+				value : 3
+			} ];
+			$scope.priority = $scope.priorities[0];
+			$scope.task.priority = $scope.priority.value;
+
 			$scope.update = function() {
 				$scope.task.categoryId = $scope.category.id;
-				}
-			
+				$scope.task.priority = $scope.priority.value;
+			}
+
 		} ]);
