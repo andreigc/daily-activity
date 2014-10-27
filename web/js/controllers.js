66136a812f478347f416bb299588e0bfadc2dc45
Andrei Geacar <andrei.geacar@gmail.com>
2014-10-27 09:58:18 +0200

    Added task type field in new-task subpage. Added basic styling for the new-task subpage.

--- web/js/controllers.js
+++ web/js/controllers.js
@@ -25,7 +25,8 @@ dailyAppcontrollers.controller('TaskNewController', [ '$scope', '$routeParams',
 			$scope.task = {}
 			if ($routeParams.parentId) {
 				$scope.task.parentId = $routeParams.parentId;
-			}
+				
+			} 
 			$cookies.sessionId = 1;
 			$http.get("rest/categories/get").success(function(data) {
 				$scope.categories = data;
@@ -45,10 +46,28 @@ dailyAppcontrollers.controller('TaskNewController', [ '$scope', '$routeParams',
 			} ];
 			$scope.priority = $scope.priorities[0];
 			$scope.task.priority = $scope.priority.value;
+			
+			$scope.taskTypes = [{
+				name : 'Standalone',
+				value: 1
+			},{
+				name: 'Container',
+				value: 2
+			}]
+			if (!$routeParams.parentId) {
+				$scope.taskType = $scope.taskTypes[0];
+				$scope.task.taskType = $scope.taskType.value;
+			}else{
+				$scope.task.taskType = 3;
+			}
 
 			$scope.update = function() {
 				$scope.task.categoryId = $scope.category.id;
 				$scope.task.priority = $scope.priority.value;
+			}
+			
+			$scope.updateTaskType = function(){
+				$scope.task.taskType = $scope.taskType.value;
 			}
 
 		} ]);
