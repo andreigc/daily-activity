60e760f2d060e02aedbdfe733f268e1760e17002
Andrei Geacar <andrei.geacar@gmail.com>
2014-11-08 15:08:03 +0200

    changed connection factory for heroku

--- src/main/java/com/andreicg/solution/dailyagenda/model/ConnectionFactory.java
+++ src/main/java/com/andreicg/solution/dailyagenda/model/ConnectionFactory.java
@@ -1,34 +1,49 @@
 package com.andreicg.solution.dailyagenda.model;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
-    private String driverName = "org.postgresql.Driver";
-    private String url = "jdbc:postgresql://localhost/dailyagenda";
-    private String user = "postgres";
-    private String password = "postgres";
+	private String driverName = "org.postgresql.Driver";
+	//private String url = "jdbc:postgresql://localhost/dailyagenda";
+	//private String user = "postgres";
+	//private String password = "postgres";
 
-    private static ConnectionFactory connectionFactory;
+	private static ConnectionFactory connectionFactory;
 
-    private ConnectionFactory() {
-	try {
-	    Class.forName(driverName);
-	} catch (ClassNotFoundException e) {
-	    e.printStackTrace();
+	private ConnectionFactory() {
+		try {
+			Class.forName(driverName);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
-    }
-    
-    public static ConnectionFactory getInstance(){
-	if(connectionFactory==null){
-	    connectionFactory = new ConnectionFactory();
+
+	public static ConnectionFactory getInstance() {
+		if (connectionFactory == null) {
+			connectionFactory = new ConnectionFactory();
+		}
+		return connectionFactory;
 	}
-	return connectionFactory;
-    }
-    
-    public Connection getConnection() throws SQLException{
-	 return DriverManager.getConnection(url, user, password);
-    }
+
+	public Connection getConnection() throws SQLException {
+
+		URI dbUri = null;
+		try {
+			dbUri = new URI(System.getenv("DATABASE_URL"));
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+
+		String username = dbUri.getUserInfo().split(":")[0];
+		String password = dbUri.getUserInfo().split(":")[1];
+		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':'
+				+ dbUri.getPort() + dbUri.getPath();
+
+		return DriverManager.getConnection(dbUrl, username, password);
+	}
 
 }
